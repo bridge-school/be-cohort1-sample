a519b18c60495911f6356b9096a96cecc6c8d322
auredufour <aurelie.dufour@tophatmonocle.com>
2020-03-04 19:59:28 -0500

    Update book data

--- src/routes/books/books.controller.js
+++ src/routes/books/books.controller.js
@@ -1,4 +1,7 @@
-const booksData = require("../../../db/books.json")
+const fs = require("fs");
+const { promisify } = require("util");
+const booksData = require("../../../db/books.json");
+const writeFile = promisify(fs.writeFile);
 
 const listBooks = (req, res) => {
     return res.json({
@@ -9,7 +12,7 @@ const listBooks = (req, res) => {
 const postBooks = async (req, res) => {
     const bookID = booksData.length + 1;
     const newBooksData = [...booksData, {...req.body, bookID }];
-
+    await writeFile("db/books.json", JSON.stringify(newBooksData));
     res.status(201)
     return res.json({
         bookID,
