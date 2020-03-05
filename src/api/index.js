af9243ec38eadae0722f4c09be0fb392536f9807
auredufour <aurelie.dufour@tophatmonocle.com>
2020-03-04 19:47:50 -0500

    Add books router to app router

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const { healthRouter } = require('../routes/health/health.router')
+const { booksRouter } = require('../routes/books/books.router')
 
 const router = express.Router();
+router.use("/books", booksRouter);
 router.use("/health", healthRouter);
+
 
 module.exports = router;
